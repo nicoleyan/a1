102c63bc6e55d66c3d53a76d9383cab225260244
nicoleyan <nicoleyaneza@trentu.ca>
2017-01-31 18:38:18 -0500

    this is a setup for assignment one

---
+++ Assignment one/src/assignone/assignment1.java
@@ -0,0 +1,10 @@
+package assignone;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Assignment one/src/assignone/test.java
@@ -0,0 +1,9 @@
+package assignone;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
